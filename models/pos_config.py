0faf1312d5ba9ef22aa1776322ccbf0baf0dfe33
fadlizarli <fadlizarli68@gmail.com>
2026-05-16 12:57:21 +0700

    remove: hapus file lama pos_config.py

--- models/pos_config.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class PosSession(models.Model):
-    _inherit = 'pos.session'
-
-    def _loader_params_product_product(self):
-        result = super()._loader_params_product_product()
-        result['search_params']['fields'].append('kode_modal')
-        return result
