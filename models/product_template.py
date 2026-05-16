b61183485c8ae60cafd0686de22719b5d94f4cf7
fadlizarli <fadlizarli68@gmail.com>
2026-05-16 12:57:07 +0700

    feat: module kode_modal_generator - replace module lama

--- models/product_template.py
+++ models/product_template.py
@@ -1,9 +1,38 @@
 from odoo import fields, models
 
+KODE_PERTAMA = {'1': 'M', '2': 'O', '3': 'B', '4': 'I', '5': 'L',  # MOBILSEDAN
+                '6': 'S', '7': 'E', '8': 'D', '9': 'A', '0': 'N'}
+KODE_KEDUA   = {'1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E',
+                '6': 'F', '7': 'G', '8': 'H', '9': 'I', '0': 'L'}
+
+
+def _encode(price, mapping):
+    price = int(round(price))
+    if price <= 0:
+        return ''
+    if price % 1_000_000 == 0:
+        number, suffix = price // 1_000_000, 'JT'
+    elif price % 1_000 == 0:
+        number, suffix = price // 1_000, 'RB'
+    elif price % 100 == 0:
+        number, suffix = price // 100, 'RT'
+    else:
+        number, suffix = price, ''
+    letters = ''.join(mapping.get(d, d) for d in str(number))
+    return (letters + ' ' + suffix) if suffix else letters
+
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     kode_modal = fields.Char(string='Kode Modal')
+
+    def action_generate_kode1(self):
+        self.kode_modal = _encode(self.standard_price, KODE_PERTAMA)
+
+    def action_generate_kode2(self):
+        self.kode_modal = _encode(self.standard_price, KODE_KEDUA)
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
@@ -11,5 +40,5 @@ class ProductProduct(models.Model):
     kode_modal = fields.Char(
         string='Kode Modal',
         related='product_tmpl_id.kode_modal',
-        store=True
+        store=True,
     )
